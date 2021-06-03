5692e92c3095893b33706b57a68e86776a75597b
Lexie Crawford <lexcraw4d@gmail.com>
2021-06-03 13:47:52 -0500

    POST functionality added now can create and add animals to the database via html form && created frontend functionality to show via server on backend

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
+//allows index.html to access it's linked assets (e.g. style.css)
+app.use(express.static('public'));
 
 function filterByQuery(query, animalsArray) {
 	let personalityTraitsArray = [];
@@ -105,6 +107,18 @@ app.get('/api/animals/:id', (req, res) => {
 	  const animal = createNewAnimal(req.body, animals);
 	  res.json(animal);
 	}
+  });
+  app.get('/', (req, res) => {
+	res.sendFile(path.join(__dirname, './public/index.html'));
+  });
+  app.get('/animals', (req, res) => {
+	res.sendFile(path.join(__dirname, './public/animals.html'));
+  });
+  app.get('/zookeepers', (req, res) => {
+	res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+  });
+  app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname, './public/index.html'));
   });
 app.listen(PORT, () => {
 	console.log(`API server now on port ${PORT}!`);
